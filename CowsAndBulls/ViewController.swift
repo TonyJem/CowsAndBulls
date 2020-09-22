206844b52a00d4db91fd7aa707753d5057ee7f77
TonyJem <62282734+TonyJem@users.noreply.github.com>
2020-09-22 17:17:56 +0300

    Fill in the Table View

--- CowsAndBulls/ViewController.swift
+++ CowsAndBulls/ViewController.swift
@@ -7,24 +7,58 @@
 
 import Cocoa
 
-class ViewController: NSViewController {
-
+class ViewController: NSViewController, NSTableViewDelegate, NSTableViewDataSource {
+    
     @IBOutlet var tableView: NSTableView!
     @IBOutlet var guess: NSTextField!
     
+    //    Correct answer
+    var answer = ""
+    
+    //    Array to store player's guesses
+    var guesses = [String]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
     }
-
+    
     override var representedObject: Any? {
         didSet {
-        // Update the view, if already loaded.
+            // Update the view, if already loaded.
         }
     }
-
+    
     @IBAction func submitGuess(_ sender: Any) {
+    }
+    
+    func numberOfRows(in tableView: NSTableView) -> Int {
+        return guesses.count
+    }
+    
+    func result (for guess: String) -> String {
+        return "Result"
+    }
+    
+    func tableView(_ tableView: NSTableView, viewFor tableColumn: NSTableColumn?, row: Int) -> NSView? {
+        guard let vw = tableView.makeView(withIdentifier: tableColumn!.identifier, owner: self) as? NSTableCellView else {
+            return nil
+        }
+        
+        if tableColumn?.title == "Guess" {
+            // this is the Guess column, show a previuos guess
+            vw.textField?.stringValue = guesses[row]
+        } else {
+            // this is the Result column, call our new method
+            vw.textField?.stringValue = result(for: guesses[row])
+        }
+        
+        return vw
+    }
+    
+    func tableView(_ tableView: NSTableView, shouldSelectRow row: Int) -> Bool {
+        return false
     }
     
 }
